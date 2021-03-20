cf6cc5a38c3a45cad2a252e15eae3a4582d715da
Mark Davydov <markrity@gmail.com>
2021-03-20 02:04:10 +0200

    Added output for private ips

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,6 +7,12 @@ output "openvpn_ec2_instance_ip" {
   value = aws_instance.openvpn.public_ip
 }
 
+output "openvpn_ec2_instance_private_ip" {
+  value = aws_instance.openvpn.private_ip
+}
+
+
+
 output "openvpn_connection_string" {
   value = "'ssh -i ${var.ssh_private_key_file} ${var.ec2_username}@${aws_instance.openvpn.public_dns}'"
 }
@@ -16,8 +22,12 @@ output "jenkins_ec2_instance_dns" {
   value = aws_instance.jenkins.public_dns
 }
 
-output "jenkins_ec2_instance_ip" {
+output "jenkins_ec2_instance_public_ip" {
   value = aws_instance.jenkins.public_ip
+}
+
+output "jenkins_ec2_instance_private_ip" {
+  value = aws_instance.jenkins.private_ip
 }
 
 output "jenkins_connection_string" {
